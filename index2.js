91b7d8b00b418f34cc4632b655e666fce26e3fec
Mahmuda826 <155256871+Mahmuda826@users.noreply.github.com>
2024-08-08 00:34:13 +0600

    Child and sibling element

---
+++ index2.js
@@ -0,0 +1,12 @@
+ Pnote = document.querySelector(".Pnote");
+console.log(Pnote.parentNode);
+get child element
+
+ let parent = document.querySelector('.parentDiv');
+console.log( parent.children);
+
+
+
+//Sibling element
+ let  current = document.querySelector(".current");
+console.log( current.nextElementSiblingSibling );
